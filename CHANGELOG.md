9f4564ab4ab5d9be20f7a7b6621b519bcd697076
Sergey Ivashchenko <sergeyiivashchenko@gmail.com>
2019-03-25 14:08:41 +0200

    added CHANGELOG.md, move app/d3 to d3-graph.module

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## Unreleased (DD-MM-YYYY)
+### Added 
+- Module d3-graph for render graph with D3 library
+### Changed 
+### Deprecated 
+### Removed 
+### Fixed 
+### Security
--- src/app/d3/model/force-directed-graph.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ForceDirectedGraph } from './force-directed-graph';
-
-describe('ForceDirectedGraph', () => {
-  it('should create an instance', () => {
-    expect(new ForceDirectedGraph([], [], { width: 1, height: 1 })).toBeTruthy();
-  });
-});
